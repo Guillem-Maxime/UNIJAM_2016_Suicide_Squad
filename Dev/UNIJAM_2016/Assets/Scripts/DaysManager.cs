f1a293a75cddebf10e340481857fe5b38b562a67
riviere2014 <a.riviere443@laposte.net>
2016-11-05 03:15:00 +0100

    J'ai fait des trucs.

--- Dev/UNIJAM_2016/Assets/Scripts/DaysManager.cs
+++ Dev/UNIJAM_2016/Assets/Scripts/DaysManager.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class DaysManagers : MonoBehaviour {
+
+    int nmbDayMAX = 9;
+    int mDay = 0;
+    Timer[] timerTab;
+    GameObject eventManager;
 
 	// Use this for initialization
 	void Start () {
-	
+        timerTab = new Timer[nmbDayMAX];
+        //eventManager = GameObject.FindGameObjectWithTag("EventManager");
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    /*
+        for tous les events du jour n° mDay
+            
+        */
 	}
+
+    public void NextDay()
+    {
+        timerTab[mDay].Stop();
+        timerTab[mDay].Reset();
+        mDay++;
+        timerTab[mDay].LetsStart();
+    }
 }
