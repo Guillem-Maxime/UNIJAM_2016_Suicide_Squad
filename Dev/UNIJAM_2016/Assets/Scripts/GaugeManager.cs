bd6f9cc114ef1863992639271d0c7498f4223330
riviere2014 <a.riviere443@laposte.net>
2016-11-05 01:39:09 +0100

    Ajout des premiers scripts vides et création du canvas.

---
+++ Dev/UNIJAM_2016/Assets/Scripts/GaugeManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GaugeManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
