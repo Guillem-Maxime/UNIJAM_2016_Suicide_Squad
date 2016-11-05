e595c2951b2b21f378998745862bd75b5545c8cc
riviere2014 <a.riviere443@laposte.net>
2016-11-05 09:57:58 +0100

    On a des jauges de fatigue et de danger qui marchent !

--- Dev/UNIJAM_2016/Assets/Scripts/GaugeManager.cs
+++ Dev/UNIJAM_2016/Assets/Scripts/GaugeManager.cs
@@ -5,17 +5,19 @@ using UnityEngine.UI;
 public class GaugeManager : MonoBehaviour {
 
     UIManager uiManager;
+
     int dangerMAX = 100;
     int danger;
     int fatigueMAX = 100;
     int fatigue;
 
-    Slider dangerSlider;
-    Slider fatigueSlider;
+    public Slider dangerSlider;
+    public Slider fatigueSlider;
 
-    DaysManager daysManager;
+    
 
 	void Start () {
+        uiManager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
         danger = dangerMAX / 2;
         fatigue = fatigueMAX / 2;
 	}
@@ -24,22 +26,37 @@ public class GaugeManager : MonoBehaviour {
 	    if (danger == 100 || fatigue == 100)
         {
             uiManager.AfficheGameOver();
-            daysManager.FirstDay();
             danger = dangerMAX / 2;
             fatigue = fatigueMAX / 2;
         }
-	}
+        dangerSlider.value = danger;
+        fatigueSlider.value = fatigue;
+    }
 
     public void AddDanger(int var)
     {
-        danger += var;
-        dangerSlider.value = danger;
+        danger += var; 
+        if (danger < 0)
+        {
+            danger = 0;
+        } 
+        else if (danger > 100)
+        {
+            danger = 100;
+        }
     }
 
     public void AddFatigue(int var)
     {
         fatigue += var;
-        fatigueSlider.value = fatigue;
+        if (fatigue < 0)
+        {
+            fatigue = 0;
+        }
+        else if (fatigue > 100)
+        {
+            fatigue = 100;
+        }
     }
 
 }
