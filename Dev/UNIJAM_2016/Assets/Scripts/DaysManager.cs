e01ad7cfbfe17f89c1f20144dde41869f9ec338c
riviere2014 <a.riviere443@laposte.net>
2016-11-05 03:42:48 +0100

    Préparation du DaysManager.

--- Dev/UNIJAM_2016/Assets/Scripts/DaysManager.cs
+++ Dev/UNIJAM_2016/Assets/Scripts/DaysManager.cs
@@ -1,33 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class DaysManagers : MonoBehaviour {
+public class DaysManager : MonoBehaviour {
 
+    float timeMaxPerDay = 150.0f;
     int nmbDayMAX = 9;
     int mDay = 0;
     Timer[] timerTab;
-    GameObject eventManager;
+    String[] prologueTab;
+    EventManager eventManager;
+    UIManager uiManager;
+    
 
 	// Use this for initialization
 	void Start () {
         timerTab = new Timer[nmbDayMAX];
+        prologueTab = new String[nmbDayMAX];
         //eventManager = GameObject.FindGameObjectWithTag("EventManager");
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    /*
-        for tous les events du jour n° mDay
-            
-        */
-	}
+	    
+        if  (timerTab[mDay].Get() > timeMaxPerDay)
+        {
+            NextDay();
+        }
+        else
+        {
+            /*
+            for (tous les events du jour n° mDay pas encore lancés) {
+                if (timerTab[mDay].Get() > eventmanager.get(i).time) {
+                    eventmanager.get(i).launch();
+                }
+            }
+            */
+        }
+    }
 
     public void NextDay()
     {
         timerTab[mDay].Stop();
         timerTab[mDay].Reset();
         mDay++;
+        //uiManager.AffichePrologue(prologueTab[mDay]);
+        timerTab[mDay].LetsStart();
+    }
+
+    public void FirstDay()
+    {
+        //uiManager.AffichePrologue(prologueTab[mDay]);
         timerTab[mDay].LetsStart();
     }
 }
