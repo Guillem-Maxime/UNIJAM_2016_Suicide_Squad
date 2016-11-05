155326946f77d44b1b8925811b1b97e5b52e8361
Guillem-Maxime <maxime.guillem@telecom-sudparis.eu>
2016-11-05 04:04:09 +0100

    debut des objects

--- Dev/UNIJAM_2016/Assets/Scripts/Object.cs
+++ Dev/UNIJAM_2016/Assets/Scripts/Object.cs
@@ -1,15 +1,47 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Object : MonoBehaviour {
 
+    private Ray ray;
+    private RaycastHit hit;
+
+    private Vector3 initialPosition;
+
+    public Vector3 getInitialPosition()
+    {
+        return initialPosition;
+    }
+
+    public LayerMask interactions;
+
 	// Use this for initialization
 	void Start () {
-	
+        initialPosition = new Vector3(0, 0, 0);
+        this.transform.position = initialPosition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        Debug.Log(Input.mousePosition);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        if (Physics.Raycast(ray, out hit, 50f))
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Debug.Log(Input.mousePosition);
+                this.transform.position = Input.mousePosition;
+            }else if(Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                this.transform.position = initialPosition;
+            }
+        }
+    }
+
 }
--- Dev/UNIJAM_2016/Assets/Scripts/ObjectManager.cs
+++ Dev/UNIJAM_2016/Assets/Scripts/ObjectManager.cs
@@ -1,15 +1,83 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+enum objectTypes { };
 
 public class ObjectManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    List<Object> listObject;
+
+    private bool isDraggingObject;
+    private Ray ray;
+    private RaycastHit hit;
+
+    public LayerMask interactions;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        detectClick();
+
+
+    }
+
+    public void addObject(Object obj)
+    {
+        listObject.Add(obj);
+    }
+
+    public void removeObject(Object obj)
+    {
+        listObject.Remove(obj);
+    }
+
+    public void removeObject(int i)
+    {
+        listObject.RemoveAt(i);
+    }
+
+    private Object searchInList(GameObject gObject)
+    {
+        Object result = new Object();
+
+        for (int i = 0; i < listObject.Count; i++)
+        {
+            if (listObject[i].gameObject == gObject)
+                result = listObject[i];
+        }
+
+        return result;
+    }
+
+    void detectClick()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        if (Physics.Raycast(ray, out hit, 50f, interactions))
+        {
+
+            Object pointedObject = searchInList(hit.collider.gameObject);
+
+            if (hit.collider.gameObject.tag == "dragable")
+            {
+                if (Input.GetKey(KeyCode.Mouse0))
+                {
+                    pointedObject.gameObject.transform.position = Input.mousePosition;
+                }
+                else
+                {
+                    pointedObject.gameObject.transform.position = pointedObject.getInitialPosition();
+                }
+
+            }
+        }
+    }
 }
