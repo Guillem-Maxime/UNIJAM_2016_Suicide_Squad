2806ed003fc24a9750f015efc30eb046cc7420c1
Guillem-Maxime <maxime.guillem@telecom-sudparis.eu>
2016-11-05 17:36:47 +0100

    ADRIENNE Drag and Dropping Working + sprite, writing, destroy

---
+++ Dev/UNIJAM_2016/Assets/Scripts/DropHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class DropHandler : MonoBehaviour, IDropHandler
+{
+    public delegate void DropDelegate(Object itemBeingDragged, Object itemCollided);
+
+    public event DropDelegate dropEvent;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+
+            if (dropEvent != null)
+            {
+                dropEvent(DragHandler.itemBeingDragged, this.GetComponent<Object>());
+            }
+        
+    }
+
+    void Start()
+    {
+       // itemDroppedOn = this.gameObject.GetComponent<Object>();
+    }
+}
