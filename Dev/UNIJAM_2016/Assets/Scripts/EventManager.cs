b32fd4944749c864107ec695f3d71b71a15cc15d
artemaus <emma.goldblum@telecom-sudparis.eu>
2016-11-05 03:17:56 +0100

    "Event, EventManager et UIManager première ebauche

--- Dev/UNIJAM_2016/Assets/Scripts/EventManager.cs
+++ Dev/UNIJAM_2016/Assets/Scripts/EventManager.cs
@@ -3,8 +3,39 @@ using System.Collections;
 
 public class EventManager : MonoBehaviour {
 
+    enum  EventRadioDay
+    {
+        radioDay1,
+        radioDay2,
+        radioDay3,
+        radioDay4,
+        radioDay5,
+        radioDay6,
+        radioDay7,
+        radioDay8
+    }
+
+    enum EventChat
+    {
+        chatArrive,
+        chatGratteALaPorte,
+        chatRepart,
+    }
+
+    enum CoupDansLaPorte
+    {
+        coupDay1,
+        coupDay2,
+        coupDay3,
+        coupDay4,
+        coupDay5,
+        coupDay6,
+        coupDay7
+    }
+
 	// Use this for initialization
-	void Start () {
+
+    void Start () {
 	
 	}
 	
--- Dev/UNIJAM_2016/Assets/Scripts/UIManager.cs
+++ Dev/UNIJAM_2016/Assets/Scripts/UIManager.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
+
+    Text superbetexte;
+
+    public void write(string phrase)
+    {
+        superbetexte.text = phrase; 
+    }
+
 	// Use this for initialization
-	void Start () {
+
+    void Start () {
 	
 	}
 	
